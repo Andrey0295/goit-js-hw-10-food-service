d1f45f140124f011f81727c71cfba2c728721b62
Andrey0295 <andreevicandrej00@gmail.com>
2020-10-24 23:26:48 +0300

    Завершает работу над д.з 10

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import './sass/main.scss';
 import './js/changeThemeFunction';
+import './js/menu-with-all-dishes';
---
+++ src/js/menu-with-all-dishes.js
@@ -0,0 +1,14 @@
+import dishesCardsTemplates from '../templates/dishes';
+import dishes from '../menu.json';
+
+console.log(dishesCardsTemplates(dishes));
+console.log(dishes);
+
+const getMenuListEl = document.querySelector('.js-menu');
+const cardsMarkup = dishesCardsTemplates(dishes);
+
+getMenuListEl.insertAdjacentHTML('beforeend', cardsMarkup);
+
+// function createDisheCardsMarkup(dishes) {
+//   return dishesCardsTemplates(dishes);
+// }
